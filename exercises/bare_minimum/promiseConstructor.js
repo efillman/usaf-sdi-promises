b8bc4155c1107949b527a19738cc9f0d01636fbd
Jo Vasquez <joanna7vasquez@gmail.com>
2021-07-12 12:39:04 -0400

    Updated promises

--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -10,12 +10,43 @@ var Promise = require('bluebird');
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  return new Promise(function (resolve, reject) {
+    fs.readFile(filePath, 'utf8', function (err, content) {
+      if (!err) {
+      let accumulator='';
+      let newLineFlag=false;
+      //content is an array of characters
+      //read the first line until /n or end of line
+      for (var i =0; i < content.length; i++){
+        if (content[i] === '\n') {
+          newLineFlag = true;
+        }
+        //add content to an accumulator until you hit a /n
+        if (!newLineFlag) {
+          accumulator += content[i];
+        }
+      }
+      //return the first line to the callback
+      resolve(accumulator);
+    } else {
+      reject(err)
+    }
+    })
+  })
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise(function (resolve, reject) {
+    request(url, function (error, response, body) {
+      if (!error) {
+        let responseData = response;
+        resolve(responseData.statusCode);
+      } else {
+        reject(error)
+      }
+    })
+  })
 };
 
 // Export these functions so we can test them and reuse them in later exercises
